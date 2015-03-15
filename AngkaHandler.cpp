abc3e1cc35f225b6673e6cb635512540cd8cfb77
Juan Anton <juan.anton.che@gmail.com>
2015-03-15 17:57:29 +0700

    Create AngkaHandler.cpp

---
+++ AngkaHandler.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include "AngkaHandler.h"
+
+AngkaHandler::AngkaHandler() : value(0) {
+}
+
+AngkaHandler::AngkaHandler(const AngkaHandler& AH) : value(AH.value) {
+}
+
+AngkaHandler::~AngkaHandler(){
+}
+
+AngkaHandler& AngkaHandler::operator=(const AngkaHandler& AH){
+    value = AH.value;
+    return *this;
+}
+
+void AngkaHandler::setvalue(std::string value) {
+    try {
+        int x = 0;
+        float f = 0;
+        while (value[x] != '\0') {
+            char test = value[x];
+            if (test != '1' && test != '0' && test != '2' && test != '3' && test != '4' && test != '5' && test != '9' && test != '.') {
+                throw -1;
+            }
+            x++;
+        }
+        x = 0;
+        f = atof(&value[x]);
+        this->value = f;
+    }
+    catch(int) {
+        this->value = 0;
+        std::cerr << "Wrong Input" << std::endl;
+    }
+}
+
+float AngkaHandler::getvalue() const {
+	return value;
+}
