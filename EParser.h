f64efd4c23e53207d79d05de280665adf25060d2
Calvin Aditya Jonathan <deus.caj@gmail.com>
2015-03-15 19:28:04 +0700

    Update EParser.h

--- EParser.h
+++ EParser.h
@@ -1,25 +1,68 @@
 #ifndef EPARSER_H
 #define EPARSER_H
 
+#include <iostream>
 #include <string>
 #include <vector>
-#include <iterator>
-#include <algorithm>
-#include <iostream>
 #include "Tree.h"
-#include "Stack.h"
 
 using namespace std;
 
 class EParser {
 public:
-    virtual ~EParser() {}
-    virtual void Tokenize(const string& str, vector<string>& tokens, const string& delimiters = " ") = 0;
-    virtual vector<string> GetInput() = 0;
-    virtual node<string> *Parse(vector<string>& input) = 0;
-    virtual void PrintParser(node<string> *leaf) = 0;
-
     Tree<string> T;
+
+    ~EParser()
+    {}
+
+    node<string> *Parse(vector<string>& input) // Membaca string prefix dan masukkan ke tree
+    {
+        string container = input[0];
+        input.erase(input.begin());
+        if (container == "+" || container == "-" || container == "*" || container == "/") {
+            node<string> *leaf;
+            leaf = new node<string>;
+            leaf->value = container;
+            leaf->left = Parse(input);
+            leaf->right = Parse(input);
+            return leaf;
+        }
+        else {
+            node<string> *leaf;
+            leaf = new node<string>;
+            leaf->value = container;
+            leaf->left = NULL;
+            leaf->right = NULL;
+            return leaf;
+        }
+    }
+
+    void PrintPrefix(node<string> *leaf)
+    {
+        if (leaf != NULL) {
+            cout << leaf->value << " " ;
+            PrintPrefix(leaf->left);
+            PrintPrefix(leaf->right);
+        }
+    }
+
+    void PrintPostfix(node<string> *leaf)
+    {
+        if (leaf != NULL) {
+            PrintPostfix(leaf->left);
+            PrintPostfix(leaf->right);
+            cout << leaf->value << " " ;
+        }
+    }
+
+    void PrintInfix(node<string> *leaf)
+    {
+        if (leaf != NULL) {
+            PrintInfix(leaf->left);
+            cout << leaf->value << " " ;
+            PrintInfix(leaf->right);
+        }
+    }
 };
 
 #endif // EPARSER_H
