102c11ee247e4fbb25d248204a91222ab4030c7e
Juan Anton <juan.anton.che@gmail.com>
2015-03-15 18:19:10 +0700

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -1,14 +1,30 @@
 #include "Prefix.h"
 #include "Postfix.h"
 #include "Infix.h"
+#include "AngkaHandler.h"
+#include "RomanHandler.h"
+#include "LogicHandler.h"
+#include <iostream>
 
+using namespace std;
 int main()
 {
     Infix A;
     Prefix B;
+    AngkaHandler A1;
+    RomanHandler B1;
+    LogicHandler C1;
+    
     vector<string> split;
     split = A.GetInput();
     A.T.root = A.Parse(split);
     B.PrintParser(A.T.root);
+    
+    A1.setvalue("420.42");
+    B1.setvalue("MMD");
+    C1.setvalue("true");
+    cout << A1.getvalue() << endl;
+    cout << B1.getvalue() << endl;
+    cout << C1.getvalue() << endl;
     return 0;
 }
