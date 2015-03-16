fbfa64a98fada66af7e095b7eee4f73cf6bca54b
freddyisman <freddyisman@gmail.com>
2015-03-17 06:41:54 +0800

    Update AngkaHandler.cpp

--- AngkaHandler.cpp
+++ AngkaHandler.cpp
@@ -19,6 +19,7 @@ AngkaHandler& AngkaHandler::operator=(const AngkaHandler& AH){
 
 void AngkaHandler::setvalue(std::string value) {
     try {
+    	int once = 0;
         int x = 0;
         float f = 0;
         while (value[x] != '\0') {
@@ -26,6 +27,10 @@ void AngkaHandler::setvalue(std::string value) {
             if (test != '0' && test != '1' && test != '2' && test != '3' && test != '4' && test != '5' && test != '6' && test != '7' && test != '8' && test != '9' && test != '.') {
                 throw -1;
             }
+            if (test == '.' && once == 1)
+            	throw -1; 
+            else if (test == '.' && once == 0) 
+            	once = 1;
             x++;
         }
         x = 0;
