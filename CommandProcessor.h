d24859f1b3fb74221f063671b2e83bf8f31f71f3
Calvin Aditya Jonathan <deus.caj@gmail.com>
2015-03-17 23:11:32 +0700

    Update CommandProcessor.h

--- CommandProcessor.h
+++ CommandProcessor.h
@@ -1,40 +1,154 @@
-#ifndef COMPROCESSOR_H
-#define COMPROCESSOR_H
-#include "Stack.h"
-#include <string>
+#ifndef COMMANDPROCESSOR_H
+#define COMMANDPROCESSOR_H
+#include "Prefix.h"
+#include "Postfix.h"
+#include "Infix.h"
+#include "EParser.h"
+#include "StdCalc.h"
+#include "LogicCalc.h"
+#include "MemoryHandler.h"
+#include <sstream>
 
-class CommandProcessor : public Stack<string> {
+class CommandProcessor {
 public:
-    CommandProcessor();
-    ~CommandProcessor();
-    void Undo();
-    void Redo();
+    CommandProcessor()
+    {
+        C = new StdCalc(1, 1);
+        In = new Prefix;
+    }
+
+    void Redo(int n)
+    {
+        if (CStack.isEmpty() == 1)
+            cout << "Nothing to redo" << endl;
+        else {
+            int i = n;
+            string dump;
+            Stack<string> temp, temp2;
+            while (CStack.isEmpty() != 1 && i > 0 ) {
+                CStack.Pop(dump);
+                temp.Push(dump);
+                i--;
+                cout << dump << endl;
+            }
+            while(temp.isEmpty() != 1) {
+                temp.Pop(dump);
+                temp2.Push(dump);
+            }
+            while(temp2.isEmpty() != 1) {
+                temp2.Pop(dump);
+                CStack.Push(dump);
+                Process(dump);
+            }
+        }
+    }
+
+    void Undo(int n)
+    {
+        if (CStack.isEmpty() == 1)
+            cout << "Nothing to undo" << endl;
+        else {
+            int i = n;
+            string dump;
+            while (CStack.isEmpty() != 1 && i > 0 ) {
+                CStack.Pop(dump);
+                i--;
+            }
+            cout << "Undo successful" << endl;
+        }
+    }
+
+    void Process(string command, int n)
+    {
+        if (command == "Show Mem") {
+            Mem.PrintNMem(n);
+            system("pause");
+        }
+        else if (command == "Redo") {
+            Redo(n);
+            ostringstream convert;
+            convert << n;
+            Mem.PushToMem(command + " " + convert.str());
+            system("pause");
+        }
+        else if (command == "Undo") {
+            Undo(n);
+            ostringstream convert;
+            convert << n;
+            Mem.PushToMem(command + " " + convert.str());
+            system("pause");
+        }
+    }
+
+    void Process(string command)
+    {
+        if (command == "Show All") {
+            Mem.PrintAll();
+            system("pause");
+        }
+        else if (command == "Save") {
+            Mem.SaveMem();
+            system("pause");
+        }
+        else {
+            if (command == "Start") {
+                cout << "Input = ";
+                string temp;
+                getline(cin, temp);
+                In->GetInput(temp);
+                input = In->GetResult();
+                EXP.StartParse(input);
+                C->StartCalc(EXP.T.root);
+                cout << "Result = " << C->GetCalcResult() << endl;
+                ostringstream convert;
+                convert << C->GetCalcResult();
+                Mem.PushToMem(temp + " = " + convert.str());
+                CStack.Push(temp + " = " + convert.str());
+                system("pause");
+            }
+            else {
+                Mem.PushToMem(command);
+                CStack.Push(command);
+                if (command == "set Prefix") {
+                    delete In;
+                    In = new Prefix;
+                }
+                else if (command == "set Postfix") {
+                    delete In;
+                    In = new Postfix;
+                }
+                else if (command == "set Infix Std") {
+                    delete In;
+                    In = new Infix(1);
+                }
+                else if (command == "set Infix Logic") {
+                    delete In;
+                    In = new Infix(2);
+                }
+                else if (command == "set Logic") {
+                    delete C;
+                    C = new LogicCalc;
+                }
+                else if (command == "set Std Arab") {
+                    delete C;
+                    C = new StdCalc(1, 1);
+                }
+                else if (command == "set Std Roman") {
+                    delete C;
+                    C = new StdCalc(2, 1);
+                }
+            }
+        }
+    }
 private:
-    int i;
-    Stack<string> temp;
-    string item;
+    Calculate *C;
+    Input *In;
+    EParser EXP;
+    vector<string> input;
+    MemoryHandler Mem;
+    Stack<string> CStack;
 };
 
-CommandProcessor::CommandProcessor(){ i = 0; }
+#endif // COMMANDPROCESSOR_H
 
-CommandProcessor::~CommandProcessor(){}
 
-void CommandProcessor::Undo(int n){
-    if(i<n && this->isEmpty()!=1){
-        this->Pop(item);
-        temp.Push(item);
-        std::cout << item + " ";
-        i++;
-    }
-}
-
-void CommandProcessor::Redo(int n){
-    if(i>=n && temp.isEmpty()!=1){
-        temp.Pop(item);
-        this->Push(item);
-        std::cout << item + " ";
-        i--;
-    }
-}
-
-#endif
