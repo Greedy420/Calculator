713fe6c7a1d56ffcfa2007f0b57aabd634cfb6c2
Juan Anton <juan.anton.che@gmail.com>
2015-03-18 01:23:51 +0700

    Update RomanHandler.h

--- RomanHandler.h
+++ RomanHandler.h
@@ -7,10 +7,11 @@ class RomanHandler : public OperandHandler {
 public :
 	RomanHandler();
 	RomanHandler(const RomanHandler& RH);
-    ~RomanHandler();
-    RomanHandler& operator=(const RomanHandler& RH);
+	~RomanHandler();
+	RomanHandler& operator=(const RomanHandler& RH);
 	void setvalue(std::string value);
 	float getvalue() const;
+	std::string returnval(float input);
 
 private :
     float value;
